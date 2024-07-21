659012fd2bb80441b15daa522003583e1571564e
Laflorentie Nicolas <laflorentie.nicolas@gmail.com>
2024-07-21 20:11:52 +0200

    Remove unnecessary middleware logging and response handlers

--- app.js
+++ app.js
@@ -41,25 +41,6 @@ app.options("*", (req, res) => {
   res.sendStatus(200);
 });
 
-app.use((req, res, next) => {
-  console.log("Requête reçue !");
-  next();
-});
-
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
-
-app.use((req, res, next) => {
-  res.json({ message: "Votre requête a bien été reçue !" });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log("Réponse envoyée avec succès !");
-});
-
 app.use("/api/auth", userRoutes);
 
 module.exports = app;
