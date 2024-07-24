bc4fc6f32e4e4c898cb989c9d2b694f12c51a82b
Laflorentie Nicolas <laflorentie.nicolas@gmail.com>
2024-07-24 17:40:01 +0200

    Add Sharp for img conversion and compression to WebP

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,20 +1,41 @@
 const multer = require("multer");
+const sharp = require("sharp");
+const path = require("path");
 
-const MIME_TYPES = {
-  "image/jpg": "jpg",
-  "image/jpeg": "jpg",
-  "image/png": "png",
+const storage = multer.memoryStorage();
+
+const upload = multer({ storage: storage }).single("image");
+
+const processImage = async (req, res, next) => {
+  if (!req.file) {
+    return next();
+  }
+
+  try {
+    const filename =
+      req.file.originalname.split(" ").join("_").split(".")[0] +
+      Date.now() +
+      ".webp";
+    const outputPath = path.join("images", filename);
+
+    await sharp(req.file.buffer)
+      .toFormat("webp")
+      .webp({ quality: 60 })
+      .toFile(outputPath);
+
+    req.file.filename = filename;
+    req.file.path = outputPath;
+    next();
+  } catch (err) {
+    next(err);
+  }
 };
 
-const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    callback(null, "images");
-  },
-  filename: (req, file, callback) => {
-    const name = file.originalname.split(" ").join("_");
-    const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + Date.now() + "." + extension);
-  },
-});
-
-module.exports = multer({ storage: storage }).single("image");
+module.exports = (req, res, next) => {
+  upload(req, res, (err) => {
+    if (err) {
+      return res.status(400).json({ error: "Error uploading file" });
+    }
+    processImage(req, res, next);
+  });
+};
--- routes/book.js
+++ routes/book.js
@@ -2,13 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const auth = require("../middleware/auth");
-const multer = require("../middleware/multer-config");
+const uploadImage = require("../middleware/multer-config");
 const bookCtrl = require("../controllers/book");
 
 router.get("/", bookCtrl.getAllBooks);
-router.post("/", auth, multer, bookCtrl.createBook);
+router.post("/", auth, uploadImage, bookCtrl.createBook);
 router.get("/:id", bookCtrl.getOneBook);
-router.put("/:id", auth, multer, bookCtrl.modifyBook);
+router.put("/:id", auth, uploadImage, bookCtrl.modifyBook);
 router.delete("/:id", auth, bookCtrl.deleteBook);
 
 module.exports = router;
