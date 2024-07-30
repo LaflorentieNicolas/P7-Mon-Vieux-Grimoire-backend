a46fd57eaa6648456e91b6ae33d93a3304f66615
Laflorentie Nicolas <laflorentie.nicolas@gmail.com>
2024-07-30 17:58:47 +0200

    Update JWT in middleware/auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,10 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    const decodedToken = jwt.verify(token, "RANDOM_TOKEN_SECRET");
+    const decodedToken = jwt.verify(
+      token,
+      "Wthngnk4/7rv+/UzucFfYu4qTX5pUKXskOtf06puqDx1nkLN++6Dr5WvgXnAgxzM"
+    );
     const userId = decodedToken.userId;
     req.auth = {
       userId: userId,
