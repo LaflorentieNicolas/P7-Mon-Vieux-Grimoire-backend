c58d7db322faef15c78d0d3dd80e40f136660faf
Laflorentie Nicolas <laflorentie.nicolas@gmail.com>
2024-08-01 23:40:02 +0200

    Add email validation to user schema

--- models/User.js
+++ models/User.js
@@ -2,7 +2,15 @@ const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 
 const userSchema = mongoose.Schema({
-  email: { type: String, required: true, unique: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [
+      /^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/,
+      "Veuillez entrer une adresse email valide",
+    ],
+  },
   password: { type: String, required: true },
 });
 
