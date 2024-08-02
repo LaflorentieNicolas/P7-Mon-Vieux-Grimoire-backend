3fb06477aedd5fcf3334b831cffecb7cf787ff37
Laflorentie Nicolas <laflorentie.nicolas@gmail.com>
2024-08-02 23:19:39 +0200

    multer-config : add filename sanitization and timestamp to uploaded images

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -2,35 +2,53 @@ const multer = require("multer");
 const sharp = require("sharp");
 const path = require("path");
 
+// Fonction pour nettoyer le nom du fichier
+const cleanString = (str) => str.replace(/[^a-zA-Z0-9_]/g, "");
+
+// Configurer le stockage en mémoire
 const storage = multer.memoryStorage();
 
+// Créez un middleware pour gérer les fichiers
 const upload = multer({ storage: storage }).single("image");
 
+// Middleware pour traiter l'image
 const processImage = async (req, res, next) => {
   if (!req.file) {
     return next();
   }
 
   try {
-    const filename =
-      req.file.originalname.split(" ").join("_").split(".")[0] +
-      Date.now() +
-      ".webp";
-    const outputPath = path.join("images", filename);
+    // Nettoyer le nom du fichier et ajouter un timestamp
+    const nameWithoutExtension = req.file.originalname
+      .split(" ")
+      .join("_")
+      .split(".")
+      .slice(0, -1)
+      .join(".");
+    const cleanedString = cleanString(nameWithoutExtension);
+    const currentTimeStamp = Date.now();
+    const fullImgName = `${cleanedString}_${currentTimeStamp}.webp`;
 
+    // Définir le chemin de sortie
+    const outputPath = path.join("images", fullImgName);
+
+    // Traiter l'image avec sharp et enregistrement
     await sharp(req.file.buffer)
       .toFormat("webp")
       .webp({ quality: 60 })
       .toFile(outputPath);
 
-    req.file.filename = filename;
+    // Ajouter le nom du fichier et le chemin au req.file
+    req.file.filename = fullImgName;
     req.file.path = outputPath;
+
     next();
   } catch (err) {
     next(err);
   }
 };
 
+// Exporter le middleware configuré
 module.exports = (req, res, next) => {
   upload(req, res, (err) => {
     if (err) {
